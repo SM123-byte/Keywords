2e5ecb87e7a47e79ee3ae0470bf19ef530bcee7d
SM123-byte <mailswagat@gmail.com>
2025-10-04 10:10:35 +0100

    Add files via upload

---
+++ break.py
@@ -0,0 +1,9 @@
+a = input("Enter a word: ")
+
+for i in a:
+    if (i == 'A' or 'a'):
+        print("A is found")
+        break
+    else:
+        print("A is not found")
+
---
+++ continue.py
@@ -0,0 +1,8 @@
+var = 11
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print("\n Current variable value: ",var)
+
+print("Happy New Year!")
